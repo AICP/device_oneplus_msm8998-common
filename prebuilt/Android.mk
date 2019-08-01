307128a1b322bb6d58ffa4fe82f9c598c1d1c6f3
root-expert <root.expert.xda@gmail.com>
2019-08-01 10:52:24 +0200

    msm8998-common: bind JamesDSP equalizer
    Change-Id: I59fbd42cb2c0017416f9ed03f6b201288daa02ca

---
+++ prebuilt/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.jamesdsp.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := common/bin/init.jamesdsp.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
