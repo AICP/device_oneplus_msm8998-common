e668f5ab576cc1a00760e23616f0a68221f12fee
Pranav Vashi <neobuddy89@gmail.com>
2020-07-12 18:47:10 +0530

    Revert "msm8998-common: Build AOSP RCS packages"
    This reverts commit ab5e87309f28061db1f0fdd05fed070ce4c2be04.
    
    Change-Id: I1cbb3962a976686b48b256d84d54d280076351e8

--- common.mk
+++ common.mk
@@ -419,9 +419,10 @@ PRODUCT_PACKAGES += \
 
 # RCS
 PRODUCT_PACKAGES += \
-    com.android.ims.rcsmanager \
-    PresencePolling \
-    RcsService
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
 
 # Recovery
 PRODUCT_PACKAGES += \
